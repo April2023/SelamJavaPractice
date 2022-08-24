b500cd041513110882844026ab90352a1df308b0
April2023 <103716654+April2023@users.noreply.github.com>
2022-08-24 18:21:18 -0400

    "Commit Selam"

--- HellowWorldProject/src/HelloWorldClass.java
+++ HellowWorldProject/src/HelloWorldClass.java
@@ -4,6 +4,8 @@ public class HelloWorldClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLOW WORLD !!!!!!");
+		
+		System.out.println("Hello Selam");
 
 	}
 
